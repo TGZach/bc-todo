df60f6209fc048bf7e8f6d3cae34ac7e54007902
Zach Herman <zwherman@yahoo.com>
2019-11-21 21:04:02 +0000

    Tasks error

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TaskController, type: :controller do
+RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
